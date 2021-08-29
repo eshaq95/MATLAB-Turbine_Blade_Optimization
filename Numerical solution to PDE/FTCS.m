642acf36b295fa87e6a70ba0ce80ca9df96e8db1
Eshaq Rahmani <72145252+eshaq95@users.noreply.github.com>
2021-08-29 20:56:16 +0100

    Delete Numerical solution to PDE directory

--- Numerical solution to PDE/FTCS.m
+++
@@ -1,7 +0,0 @@
-function c1 = FTCS(c0,dt,dx)
-kappa = 0.1;
-n = size(c0,2);
-Rx = kappa*dt/(dx^2);
-c1 = c0;
-c1(2:n-1) = c0(2:n-1)+Rx*(c0(1:n-2)-2*c0(2:n-1)+c0(3:n));
-end 
--- Numerical solution to PDE/analytical_solution.m
+++
@@ -1,8 +0,0 @@
-function T = analytical_solution(time,r,N)
-kappa = 0.1;
-T = 2*r*time/pi + cos(r)*exp(-kappa*time);
-for i = 1:N
-    T = T + (1/(2*kappa*pi))*((-1)^i/i^3)*(1-exp(-4*kappa*i^2*time))*sin(2*i*r);
-end 
-end 
-
